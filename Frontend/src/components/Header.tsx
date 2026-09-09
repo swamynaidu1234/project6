58d982f01a2689690f31c643603d8d19b8977a47
abu-umer2 <kh.mohmed97@yahoo.com>
2026-09-09 08:46:26 +0530

    Changes

--- Frontend/src/components/Header.tsx
+++ Frontend/src/components/Header.tsx
@@ -1,10 +1,14 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useState } from "react";
 import { getCat, getSubCat } from "../services/cat_subcat_service";
+import { Link } from "raviger";
 export default function Header() {
+  var [catItems, updateMenu] = useState([]);
+  var [subCatItems, updateSubcat] = useState([]);
   useMemo(() => {
     const catdata = getCat();
     catdata.then(
       (dt) => {
+        updateMenu(dt.data);
         console.log(dt.data);
       },
 
@@ -16,13 +20,13 @@ export default function Header() {
     );
     const subcatData = getSubCat();
     subcatData.then((dt) => {
-      console.log(dt.data);
+      updateSubcat(dt.data);
     });
   }, []);
   return (
-    <div >
-  <div className="navbar">
-  <a href="#home">Home</a>
+    <div>
+      <div className="navbar">
+        {/* <a href="#home">Home</a>
   <div className="dropdown">
     <button className="dropbtn">Dropdown 
       <i className="fa fa-caret-down"></i>
@@ -32,8 +36,30 @@ export default function Header() {
       <a href="#">Link 2</a>
       <a href="#">Link 3</a>
     </div>
-  </div> 
-</div>
+  </div>  */}
+        {catItems.map((catelement: any, index) => {
+          return (
+            <>
+              {/* <a key={index}></a> */}
+              <div className="dropdown">
+                <button className="dropbtn">
+                  {catelement.catName}
+                  <i className="fa fa-caret-down"></i>
+                </button>
+                <div className="dropdown-content">
+                  {subCatItems.map((subcatelement:{subCatName:string,catId:string},index)=>{
+                    if(subcatelement.catId === catelement._id)
+                    return <>
+                    <Link href="./products" key="index">{subcatelement.subCatName}</Link>
+                    </>
+                  })}
+                  
+                </div>
+              </div>
+            </>
+          );
+        })}
+      </div>
     </div>
   );
 }
--- Frontend/src/router.tsx
+++ Frontend/src/router.tsx
@@ -1,9 +1,11 @@
 import { useRoutes,Link } from "raviger";
 import Login from "./components/login";
 import Cnt from "./components/Content";
+import Products from "./components/Products";
 const obj={
     "/": ()=><Cnt />,
     "/login":()=><Login />,
+    "/products":()=><Products />
 }
 
 export default function RouterObj(){
